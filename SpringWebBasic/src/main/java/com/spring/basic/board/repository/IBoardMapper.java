903c78e44befb9569b0ddc90f8d4b5ba1b4e68ea
najaeseong <skwotjd5576@gmail.com>
2023-10-10 16:03:03 +0900

    2023-10-10 SpringWebBasic 완료?

--- SpringWebBasic/src/main/java/com/spring/basic/board/repository/IBoardMapper.java
+++ SpringWebBasic/src/main/java/com/spring/basic/board/repository/IBoardMapper.java
@@ -5,14 +5,26 @@ import java.util.List;
 import com.spring.basic.board.entity.Board;
 
 public interface IBoardMapper {
+	
 	//게시글 등록
 	void insertArticle(Board board);
+	
 	//게시글 목록
 	List<Board> getArticles();
+	
 	//게시글 상세
 	Board getArticle(int bno);
+	
 	//게시글 수정
 	void updateArticle(Board board);
+	
 	//게시글 삭제
 	void deleteArticle(int bno);
+
 }
+
+
+
+
+
+
